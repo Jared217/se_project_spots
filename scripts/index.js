b6284ad334e4d20b59f6712493bd3cc78f4b4bf7
Jared Ware <jaredware217@gmail.com>
2024-08-18 22:33:54 -0500

    file path fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Training with the boys",
-    link: "../images/training.jpg",
+    link: "./images/training.jpg",
   },
   {
     name: "Best Bud",
-    link: "../images/wesley.jpg",
+    link: "./images/wesley.jpg",
   },
   {
     name: "Newest Passion",
-    link: "../images/deskview.JPG",
+    link: "./images/deskview.JPG",
   },
   {
     name: "Living in the fast lane",
-    link: "../images/flyin.jpg",
+    link: "./images/flyin.jpg",
   },
   {
     name: "Life requires a leap of courage",
-    link: "../images/annie.jpg",
+    link: "./images/annie.jpg",
   },
   {
     name: "My first build",
-    link: "../images/machine.JPG",
+    link: "./images/machine.JPG",
   },
 ];
 
